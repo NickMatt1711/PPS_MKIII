e2116e71ef2aaf4341593f23cebb703f27ede058
NickMatt1711 <nickmatt1711@gmail.com>
2025-11-17 18:25:43 +0530

    Add global constants and helpers in constants.py

---
+++ constants.py
@@ -0,0 +1,12 @@
+# constants.py
+"""
+Global constants and small helpers.
+"""
+
+DEFAULT_TIME_LIMIT_MIN = 10
+DEFAULT_STOCKOUT_PENALTY = 10
+DEFAULT_TRANSITION_PENALTY = 10
+DEFAULT_NUM_SEARCH_WORKERS = 8
+
+# For detecting transition sheets
+TRANSITION_KEYWORD = "transition"
