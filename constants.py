ac40c0500824d9a57c98cac296caa9ad934272df
NickMatt1711 <nickmatt1711@gmail.com>
2025-11-17 20:49:58 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -3,11 +3,15 @@
 Global constants and small helpers.
 """
 
+DEFAULT_TIME_LIMIT_MIN = 10
 DEFAULT_STOCKOUT_PENALTY = 10
 DEFAULT_TRANSITION_PENALTY = 10
-DEFAULT_TIME_LIMIT = 10
-DEFAULT_BUFFER_DAYS = 3  # Added buffer days constant
 DEFAULT_NUM_SEARCH_WORKERS = 8
 
 # For detecting transition sheets
 TRANSITION_KEYWORD = "transition"
+
+# NEW: Enhanced penalty constants
+DEFAULT_MIN_INVENTORY_PENALTY = 5
+DEFAULT_MIN_CLOSING_PENALTY = 8
+DEFAULT_BUFFER_DAYS = 3
