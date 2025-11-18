8226902bbc62a7e55bce1086d2ccacd48891a03e
NickMatt1711 <nickmatt1711@gmail.com>
2025-11-18 22:19:57 +0530

    Update solver_cp_sat.py

--- solver_cp_sat.py
+++ solver_cp_sat.py
@@ -439,19 +439,16 @@ def _add_run_length_constraints(model: cp_model.CpModel, variables: Dict, instan
                 for k in range(min_run):
                     if d + k < num_days:
                         next_assign = variables['assign'].get((line, d + k, grade))
-                        if next_assign:
+                        if next_assign is not None:  
                             run_days.append(next_assign)
                 
                 if len(run_days) >= min_run:
                     for k in range(min_run):
                         if d + k < num_days:
                             next_assign = variables['assign'].get((line, d + k, grade))
-                            for i in range(len(variables.assignments) - 1):
-                                current_assign = variables.assignments[i]
-                                next_assign = variables.assignments[i + 1]
+                            if next_assign is not None:
+                                model.Add(next_assign == 1).OnlyEnforceIf(is_start)
                                 
-                                # Add constraints that only apply when certain conditions are met
-                                model.Add(variable_you_care_about == some_value).OnlyEnforceIf([current_assign, next_assign])
                 elif run_days:
                     # Can't satisfy min run, so can't start here
                     model.Add(is_start == 0)
